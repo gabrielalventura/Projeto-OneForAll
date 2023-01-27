7ab3be12aa14c9d7af9385cfa8fdf117f3f5ac0c
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-01-27 12:14:47 -0300

    Fix: Formatação do arquivo

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT
   username AS 'usuario',
-  IF(YEAR(MAX(h.history_inf)) <2021,
+  IF(YEAR(MAX(h.history_inf)) < 2021,
   'Usuário inativo', 'Usuário ativo') AS 'status_usuario'
 FROM SpotifyClone.profiles 
     INNER JOIN SpotifyClone.reproduction_history AS h ON profiles.profile_id = h.profile_id
