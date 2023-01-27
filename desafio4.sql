53303c63180fbdd5709c46b8eded5e9b21a597cc
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-01-27 12:14:15 -0300

    Execute: Exibe os usuarios ativos e inativos considerando como ano inicial 2021

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT
+  username AS 'usuario',
+  IF(YEAR(MAX(h.history_inf)) <2021,
+  'Usuário inativo', 'Usuário ativo') AS 'status_usuario'
+FROM SpotifyClone.profiles 
+    INNER JOIN SpotifyClone.reproduction_history AS h ON profiles.profile_id = h.profile_id
+    GROUP BY username ORDER BY username;
